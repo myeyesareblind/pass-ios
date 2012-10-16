7652616e88599c770fb5627fc0f18e9e08f6acdf
Brian Mattern <rephorm@rephorm.com>
2012-10-15 20:04:09 -0700

    include GPL copyright notices

--- PassDataController.h
+++ PassDataController.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2012  Brian A. Mattern <rephorm@rephorm.com>.
+ * All Rights Reserved.
+ * This file is licensed under the GPLv2+.
+ * Please see COPYING for more information
+ */
 @class PassEntry;
 
 @interface PassDataController : NSObject
--- main.m
+++ main.m
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2012  Brian A. Mattern <rephorm@rephorm.com>.
+ * All Rights Reserved.
+ * This file is licensed under the GPLv2+.
+ * Please see COPYING for more information
+ */
 int main(int argc, char **argv) {
 	NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
 	int ret = UIApplicationMain(argc, argv, @"passwordstoreApplication", @"passwordstoreApplication");
